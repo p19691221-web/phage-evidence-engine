c8cff451f688f574ac845a1fd94745cab51afe1a
p19691221-web <p19691221@gmail.com>
2026-08-28 22:24:45 +0800

    Add PHAGE v6 PQC package skeleton

---
+++ security/pqc/__init__.py
@@ -0,0 +1,6 @@
+"""
+PHAGE v6 identity and post-quantum cryptography interface.
+
+Current cryptographic implementation status:
+prototype / simulation pending production PQC backend integration.
+"""
